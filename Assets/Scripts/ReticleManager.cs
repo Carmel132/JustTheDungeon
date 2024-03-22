fae917d40e76c8122184a5ad4f2b0c0e65a8fd48
Carmel <carmelbarkaee032708@gmail.com>
2024-03-21 20:31:41 -0400

    Added reticle to cursor

---
+++ Assets/Scripts/ReticleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ReticleManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+#if UNITY_EDITOR
+        Cursor.visible = Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Handles.GetMainGameViewSize().x - 1 || Input.mousePosition.y >= Handles.GetMainGameViewSize().y - 1;
+#else
+    Cursor.visible = Input.mousePosition.x == 0 || Input.mousePosition.y == 0 || Input.mousePosition.x >= Screen.width - 1 || Input.mousePosition.y >= Screen.height - 1;
+#endif
+        transform.position = (Vector3)(Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+}
