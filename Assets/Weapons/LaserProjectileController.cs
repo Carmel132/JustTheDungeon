f89989edaee1f9dfc32090f87fb80987be2f2de1
Carmel <carmelbarkaee032708@gmail.com>
2024-05-02 20:34:01 -0400

    Laser projectiles collide with walls

--- Assets/Weapons/LaserProjectileController.cs
+++ Assets/Weapons/LaserProjectileController.cs
@@ -16,8 +16,8 @@ public class LaserProjectileController : MonoBehaviour
     {
         lineRenderer = GetComponent<LineRenderer>();
         lifetime.Reset();
-        hit = Physics2D.BoxCast(transform.position, new(0.2f, 1), Vector2.Angle(transform.position, target), target, 50, 1 << 3);
-        positions = new Vector3[] { transform.position, (transform.position + target * (hit.collider != null && hit.collider.gameObject.CompareTag("Enemy") ? Vector2.Distance(transform.position, hit.point) + 0.1f : 20)) };
+        hit = Physics2D.BoxCast(transform.position, new(0.2f, 1), Vector2.Angle(transform.position, target), target, 50, 1 << 3 | 1<<7);
+        positions = new Vector3[] { transform.position, (transform.position + target * (hit.collider != null/* && hit.collider.gameObject.CompareTag("Enemy") */? Vector2.Distance(transform.position, hit.point) + 0.1f : 20)) };
         Draw();
         // TODO: Remove when implemented recurring hitbox component
         if (hit.collider != null)
